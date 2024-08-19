c9c83029781dcfd3bf0587a4c841f672c09efc98
Logan New <157647984+Logan-New@users.noreply.github.com>
2024-08-19 13:01:41 -0400

    fixing deploment errors

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,13 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   host: process.env.DB_HOST,
   dialect: 'postgres',
   port: process.env.DB_PORT || 5432,
+  dialectOptions: {
+    ssl: {
+      require: true, // This ensures that SSL is used
+      rejectUnauthorized: false // Adjust based on your security needs
+    }
+  },
+  logging: false // Disable logging if desired
 });
 
 module.exports = sequelize;
